5461073ad192f515bc4aa41b6b2ef568323c128d
Steve Zimmers <steve@soccer-1.com>
2017-12-14 13:59:05 -0500

    more searchbar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {term: ''};
+
+		this.handleInputChange = this.handleInputChange.bind(this);
+	}
+
+	handleInputChange(event) {
+		this.setState({term: event.target.value});
+	}
+
 	render() {
 		return (
 			<form className="input-group">
-				<input />
+				<input
+					placeholder="Search city to get its forecast"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.handleInputChange}
+				/>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">Submit</button>
 				</span>
