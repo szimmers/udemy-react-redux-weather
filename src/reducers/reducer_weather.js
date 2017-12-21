76143e189c194c2a76932735b11941c2664b999b
Steve Zimmers <steve@soccer-1.com>
2017-12-21 14:50:48 -0500

    weather reducer handles data

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,14 @@
-export default function(state = null, action) {
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action) {
+	switch (action.type) {
+		case FETCH_WEATHER:
+			// concat returns a new array
+			//return state.concat([action.payload.data]);
+			// must return a new array from here
+			return [action.payload.data, ...state];
+			break;
+	}
+
 	return state;
 }
