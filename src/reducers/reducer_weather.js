ae5aa4073bf0ad3f38338d1cd722641500d29501
Steve Zimmers <steve@soccer-1.com>
2018-01-09 12:53:34 -0500

    add comment about ES6 array behaviour

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,8 +5,11 @@ export default function(state = [], action) {
 		case FETCH_WEATHER:
 			// concat returns a new array
 			//return state.concat([action.payload.data]);
+			// this is the actual equivalent of the concat
+			//console.log([...state, action.payload.data]);
 			// must return a new array from here
 			return [action.payload.data, ...state];
+
 			break;
 	}
 
